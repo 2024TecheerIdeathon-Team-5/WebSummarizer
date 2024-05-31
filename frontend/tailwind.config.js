aca98e23e04d8caf028fc89c4015a167aa782337
antisdun <112616257+antisdun@users.noreply.github.com>
2024-06-01 03:12:41 +0900

    design: 폴더 퍼블리싱

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['NanumSquare_acB', 'sans-serif'],
+        sans: ["NanumSquare_acB", "sans-serif"],
       },
       colors: {
-        primary: '#EBECFA',
-        secondary: '#1E215B',
-        tertiay: 'A6A7B5',
-      }
+        primary: "#EBECFA",
+        secondary: "#1E215B",
+        tertiay: "#A6A7B5",
+      },
     },
   },
   plugins: [],
