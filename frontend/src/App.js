162cc6a0962f7917dbdc2abe91211c85b8b58c95
antisdun <112616257+antisdun@users.noreply.github.com>
2024-06-01 04:15:25 +0900

    design: 메인 페이지 퍼블리싱 중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import Header from "./component/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Routes>
+        <Route path="/Main" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
